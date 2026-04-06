0ea9d3b871cab70b95e6be180674ef2752f6671e
pi-sentinel <pi-sentinel@local>
2026-04-06 20:59:15 -0300

    feat: add main daemon loop and set subcommand

---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gueva/pi-sentinel/alert"
+	"github.com/gueva/pi-sentinel/config"
+	"github.com/gueva/pi-sentinel/monitor"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		printUsage()
+		os.Exit(1)
+	}
+	switch os.Args[1] {
+	case "daemon":
+		runDaemon()
+	case "set":
+		if len(os.Args) != 4 {
+			fmt.Fprintln(os.Stderr, "usage: pi-sentinel set <key> <value>")
+			os.Exit(1)
+		}
+		if err := config.Set(os.Args[2], os.Args[3]); err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("set %s = %s\n", os.Args[2], os.Args[3])
+	default:
+		printUsage()
+		os.Exit(1)
+	}
+}
+
+func printUsage() {
+	fmt.Println("usage: pi-sentinel <command>")
+	fmt.Println("  daemon          start monitoring loop")
+	fmt.Println("  set <key> <val> update a config value")
+}
+
+func runDaemon() {
+	cfg, err := config.Load()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to load config: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Write config file on first run so user can find and edit it
+	if err := config.Save(cfg); err != nil {
+		fmt.Fprintf(os.Stderr, "warning: could not write config: %v\n", err)
+	}
+
+	fmt.Printf("[%s] pi-sentinel started (poll every %ds, config: %s)\n",
+		time.Now().Format("2006-01-02 15:04:05"),
+		cfg.Poll.IntervalSeconds,
+		config.ConfigPath())
+
+	dispatcher := alert.New()
+	ticker := time.NewTicker(time.Duration(cfg.Poll.IntervalSeconds) * time.Second)
+	defer ticker.Stop()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// Run first tick immediately
+	tick(dispatcher)
+
+	for {
+		select {
+		case <-ticker.C:
+			// Re-read config each tick so live edits take effect
+			cfg, _ = config.Load()
+			// Reset ticker if interval changed
+			ticker.Reset(time.Duration(cfg.Poll.IntervalSeconds) * time.Second)
+			tick(dispatcher)
+		case <-quit:
+			fmt.Printf("[%s] pi-sentinel stopped\n", time.Now().Format("2006-01-02 15:04:05"))
+			return
+		}
+	}
+}
+
+func tick(d *alert.Dispatcher) {
+	cfg, err := config.Load()
+	if err != nil {
+		return
+	}
+	var results []monitor.Result
+	results = append(results, monitor.CheckCPU(cfg)...)
+	results = append(results, monitor.CheckMemory(cfg)...)
+	results = append(results, monitor.CheckDisk(cfg)...)
+	results = append(results, monitor.CheckServices(cfg)...)
+	results = append(results, monitor.CheckNetwork(cfg)...)
+	results = append(results, monitor.CheckTemperature(cfg)...)
+
+	for _, r := range results {
+		d.Process(r, cfg)
+	}
+}
